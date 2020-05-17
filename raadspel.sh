384d1692f60d1d12a9238df237aec4a083d42c73
Rune Samyn <rune.samyn@gmail.com>
2020-05-17 10:30:08 +0100

    add 3 attempts and scoreboard

--- raadspel.sh
+++ raadspel.sh
@@ -2,11 +2,29 @@
 # opdracht 2 T Embeded Systems
 # raadspel
 
+read -p 'geef je naam in ' -a name
+date=`date '+%m-%d-%Y %H:%M:%S'`
+# generate random number between 1 and 10
 getal=$(($RANDOM % 10 + 1))
-read -p 'Raad welk getal het is ' -a guess
+# getal=8
 
-if [[ "$getal" -eq "$guess" ]];then
-	echo "true"
-else
-	echo "false"
-fi
+for i in 1 2 3; do
+	# ask the player for a number
+	read -p 'Raad welk getal het is ' -a guess
+
+	if [[ "$getal" -eq "$guess" ]];then
+		echo '==========================' >> scoreboard.txt
+		echo "name = $name" >> scoreboard.txt
+		echo "date = $date" >> scoreboard.txt
+		echo "success" >> scoreboard.txt
+		echo "attempts = $i" >> scoreboard.txt
+		echo '==========================' >> scoreboard.txt
+		break
+	elif [[ "$getal" -gt "$guess" ]];then
+		echo "greater"
+	elif [[ "$getal" -lt "$guess" ]];then
+		echo "less"
+	fi
+done
+echo $getal
+echo $guess
